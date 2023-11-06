7a02aa8f431c70f311ee4aae2f50d017e221ad35
Tom Broomfield <tomplbroomfield@gmail.com>
2023-11-07 10:55:30 +1100

    Initial commit

---
+++ spec/seq_scanner_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SeqScanner do
+  it "has a version number" do
+    expect(SeqScanner::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
